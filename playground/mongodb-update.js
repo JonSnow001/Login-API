593af77cc37b5e94345b8dd2c1fcea1bdd206615
Shubham Tiwari <subh.4256@gmail.com>
2019-03-09 23:24:06 +0530

    Add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -10,11 +10,16 @@ MongoClient.connect('mongodb://localhost:27017/Login', (err, client) => {
     console.log('Succesfully connected to MongoDB server');
   }
   const db = client.db('Login');
-  db.collection('Student').find()
-    .toArray().then((docs) => {
-      console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) => {
-      console.log('Unable to fetch Login', err);
-    });
+  db.collection('Student').findOneAndUpdate({
+    _id: new ObjectID('5c5e9a00274b333d4c1f7d3d')
+  }, {
+    $set: {
+      text: 'Shubham Tiwari'
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
   //client.close();
 });
